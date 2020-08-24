b88616231d1a57c7107ce2fce6bd0ac74276dd83
“tatianamih” <tmihalchenko66@gmail.com>
2020-08-24 23:14:57 +0300

    second task about human

---
+++ exercise2_human/src/main/java/by/jrr/jis4/service/HumanService.java
@@ -0,0 +1,12 @@
+package by.jrr.jis4.service;
+
+import by.jrr.jis4.bean.Human;
+
+public class HumanService {
+
+    public String greet(Human human){
+
+        return "Hi! My name is " + human.getName() + ", I'm " + human.getAge() + " years old";
+    }
+
+}
